1ddb29d8f4a78d6c12264a8f596ec34e77e53e8b
BittersweetPuff <biszkoptex@gmail.com>
2020-08-05 21:39:46 +0200

    Calculator working

--- frontend/go-vue/src/components/HelloWorld.vue
+++ frontend/go-vue/src/components/HelloWorld.vue
@@ -1,58 +1,99 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container>
+    <v-row>
+
+        <v-col cols="12" sm="6">
+          <v-text-field
+            v-model="num1"
+            label="Number 1"
+            outlined
+            shaped
+          ></v-text-field>
+        </v-col>
+
+        <v-col cols="12" sm="6">
+          <v-text-field
+            v-model="num2"
+            label="Number 2"
+            filled
+            shaped
+          ></v-text-field>
+        </v-col>
+
+      </v-row>
+
+
+      <div class="text-center">
+          <v-btn rounded color="primary" dark v-on:click="postreq()">Calculate</v-btn>
+      </div>
+
+      <v-row>
+
+          <v-text-field
+            v-model="add"
+            value=""
+            label="Sum"
+            filled
+            shaped
+            disabled
+          ></v-text-field>
+          <v-text-field
+            v-model="sub"
+            value=""
+            label="Subtraction"
+            filled
+            shaped
+            disabled
+          ></v-text-field>
+          <v-text-field
+            v-model="mul"
+            value=""
+            label="Multiplication"
+            filled
+            shaped
+            disabled
+          ></v-text-field>
+          <v-text-field
+            v-model="div"
+            value=""
+            label="Division"
+            filled
+            shaped
+            disabled
+          ></v-text-field>
+
+      </v-row>
+  </v-container>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+import axios from 'axios';
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+  export default {
+    name: 'HelloWorld',
+
+      data: function() {
+      return {
+        add: "", mul: "", sub: "", div: "", 
+        num1: "", num2: ""
+      }
+  },
+  methods: {
+    postreq: function() {
+      var data = {"num1": parseFloat(this.num1), "num2": parseFloat(this.num2)}
+
+            console.log(data) 
+
+      axios({ method: "POST", url: "http://127.0.0.1:8090/calc", data: data, headers: {"content-type": "text/plain" } }).then(result => { 
+        this.add = result.data['add']
+        this.mul = result.data['mul']
+        this.sub = result.data['sub']
+        this.div = result.data['div']
+      }).catch( error => {
+            console.error(error);
+      });
+
+    }
+  }
+  }
+</script>
